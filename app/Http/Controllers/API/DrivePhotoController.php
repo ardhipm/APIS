2a05dcca3ddf2f8754b69287f860bee5051e673d
ardhipm <33706825+ardhipm@users.noreply.github.com>
2022-02-06 23:47:27 +0700

    Update

--- app/Http/Controllers/API/DrivePhotoController.php
+++ app/Http/Controllers/API/DrivePhotoController.php
@@ -1202,4 +1202,54 @@ class DrivePhotoController extends Controller
         
         
     }
+    
+    public function saveOriginToDB(){
+        $customer = DB::table('customers')
+        ->leftJoin('users', 'customers.id_user', 'users.id')
+        ->leftJoin('packages', 'customers.id', 'packages.id_customer')
+        ->select('customers.id','customers.id_user', 'users.email', 'customers.name', 'customers.phone_no', 'customers.partner_name', 'packages.id as package_id')
+        ->where('customers.id_user', '=', Auth::Id())
+        ->get()->toArray();
+
+        DB::delete('DELETE from origin_photo where id_customer = '.$customer[0]->id);
+
+        $sub_package = DB::table('sub_packages')->where('id_package', '=', $customer[0]->package_id)->get()->toArray();
+        // die(print_r($sub_package));
+        
+        $folder = $customer[0]->id .' - ' .$customer[0]->name;
+
+        $contents = collect(\Storage::cloud()->listContents('/', false));
+        $dir = $contents->where('type', '=', 'dir')
+                ->where('filename', '=', $folder)
+                ->first(); // There could be duplicate directory names!
+        
+        $contents = collect(\Storage::cloud()->listContents($dir['path'], false));
+        $dir = $contents->where('type', '=', 'dir')
+        ->where('filename', '=', 'Foto Mentah')
+        ->first(); // There could be duplicate directory names!
+
+        $filedir = collect(\Storage::cloud()->listContents($dir['path'], false));
+        $directory = $filedir->where('type', '=', 'dir')->toArray();
+        // $file= $filedir->where('type', '=', 'file')->toArray();
+
+        $data = array();
+
+        for($i = 0; $i < count($directory); $i++){
+
+            $filedirchild = collect(\Storage::cloud()->listContents($directory[$i]['path'], false))->where('type', '=', 'file')->toArray();
+
+            for($j = 0; $j < count($filedirchild); $j++){
+                $bodyJson['id_customer'] = $customer[0]->id;
+                $bodyJson['sub_package_name'] = $directory[$i]['filename'];
+                $bodyJson['filename'] = $filedirchild[$j]['filename'];
+                $bodyJson['path'] = $filedirchild[$j]['path'];
+                $bodyJson['basename'] = $filedirchild[$j]['basename'];
+
+                Origin::create($bodyJson);
+
+            } 
+        }
+
+        return response(['success' => true, 'message' => 'Synchronize Successfully']);
+    }
 }
