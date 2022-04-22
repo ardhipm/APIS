c9d709816bc3a8b82a629597c87b1615532e7747
danniyp <danni.yoga@gbsystem.co.id>
2022-04-22 13:37:32 +0700

    fix origin_photo join subquery

--- routes/api.php
+++ routes/api.php
@@ -235,20 +235,32 @@ Route::get('drive/get_origin_photo_test', 'API\DrivePhotoController@getOriginPho
 Route::middleware('auth:api')->group(function () {
     Route::get('/get_origin_photo/subpackage/{id}', function ($id) {
         $customer = Customer::where('id_user', '=', Auth::id())->get()->first();
+        $joinedTbl = DB::table('selected_photo as sp')->where('sp.id_subpackage', '=', $id);
+        // $tbl = DB::table('origin_photo as op')
+        // ->leftJoin(
+        //     '(select basename, 
+        //     choice_basename, 
+        //     id_subpackage from selected_photo 
+        //     where id_subpackage = '.$id.') as sp', 
+        //     'op.basename', 
+        //     'sp.basename')
+        // ->select('op.id','op.sub_package_id', 'op.sub_package_name', 'op.filename', 'op.path', 'op.basename', 'op.id_customer', 'sp.choice_basename',
+        //     DB::raw('(CASE WHEN sp.basename is null THEN false ELSE true END) AS is_selected')
+        // )
+        // ->where('op.id_customer', '=', $customer->id)
+        // ->where('op.sub_package_id', '=', $id)->paginate(50);
+        // ->orWhere('sp.id_subpackage', '=', $id)
+
         $tbl = DB::table('origin_photo as op')
-        ->leftJoin(
-            '(select basename, 
-            choice_basename, 
-            id_subpackage from selected_photo 
-            where id_subpackage = '.$id.') as sp', 
-            'op.basename', 
-            'sp.basename')
-        ->select('op.id','op.sub_package_id', 'op.sub_package_name', 'op.filename', 'op.path', 'op.basename', 'op.id_customer', 'sp.choice_basename',
+        ->leftJoinSub($joinedTbl, 'sp', function($join){
+            $join->on('op.basename','=', 'sp.basename');
+        })
+        ->select('op.id','op.sub_package_id', 'op.sub_package_name', 'op.filename', 'op.path', 'op.basename', 'op.id_customer', 
             DB::raw('(CASE WHEN sp.basename is null THEN false ELSE true END) AS is_selected')
         )
         ->where('op.id_customer', '=', $customer->id)
-        ->where('op.sub_package_id', '=', $id)->paginate(50);
-        // ->orWhere('sp.id_subpackage', '=', $id)
+        ->where('op.sub_package_id', '=', $id)->get();
+
         return $tbl;
     });
 });
