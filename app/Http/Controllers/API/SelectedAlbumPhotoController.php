0d20f2aecd31c568489140980936b10aaf675cb6
dannpa0 <yogadanni0@gmail.com>
2022-05-18 21:34:05 +0700

    fix get()

--- app/Http/Controllers/API/SelectedAlbumPhotoController.php
+++ app/Http/Controllers/API/SelectedAlbumPhotoController.php
@@ -77,7 +77,7 @@ class SelectedAlbumPhotoController extends Controller
             ->select('sp.id')
             ->join('packages as p', 'p.id', 'sp.id_package')
             ->join('customers as c', 'c.id', 'p.id_customer')
-            ->where('c.id','=', $customer->id)->get()->toArray())->get()->count();
+            ->where('c.id','=', $customer->id)->get()->toArray())->get();
         return response(['success' => true,'data'=>$tbl, 'message' => 'Synchronize Successfully']);
     }
 }
